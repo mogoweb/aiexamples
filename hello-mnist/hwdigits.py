610d0df1fb2738ed9949c621dd939c8a86c0be17
mogoweb <mogoweb@gmail.com>
2018-07-05 21:42:45 +0800

    modify deprecated call code

--- hello-mnist/hwdigits.py
+++ hello-mnist/hwdigits.py
@@ -14,7 +14,7 @@ b = tf.Variable(tf.zeros([10]))
 
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder(tf.float32, [None, 10])
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), 1))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 tf.global_variables_initializer().run()
