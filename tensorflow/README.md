fdb0a83dd70a143816d6bc4714648cf7260a3aa4
mogoweb <mogoweb@gmail.com>
2018-10-16 14:11:28 +0800

    adjust project layout and add README

--- tensorflow/README.md
+++ tensorflow/README.md
@@ -6,7 +6,7 @@
 
 * saved_model
 
-示例说明SavedModel格式模型的保存与加载。
+  示例说明SavedModel格式模型的保存与加载。
 
   * save_model.py - 保存一个简单的手写识别模型到当前目录的./model下
   * load_model.py - 加载save_model.py所保存的位于./model下的模型，并进行预测。
@@ -14,3 +14,10 @@
   * connect_model.py - 连接两个Tensorflow模型，组成一个新的模型并保存
   * test_connect_model.py - 测试connect_model.py的新模型
 
+* mnist
+
+  手写数字识别的示例代码。
+
+  * hwdigits.py - 采用线性回归模型实现手写数字识别
+  * cnn_mnist.py - 采用卷积神经网络实现手写数字识别
+  * mlp_minist.py - 采用多层全连接网络实现手写数字识别
---
+++ tensorflow/mnist/mlp_mnist.py
@@ -0,0 +1,33 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+sess = tf.InteractiveSession()
+
+in_units = 784
+h1_units = 300
+W1 = tf.Variable(tf.truncated_normal([in_units, h1_units], stddev=0.1))
+b1 = tf.Variable(tf.zeros([h1_units]))
+W2 = tf.Variable(tf.zeros([h1_units, 10]))
+b2 = tf.Variable(tf.zeros([10]))
+
+x = tf.placeholder(tf.float32, [None, in_units])
+keep_prob = tf.placeholder(tf.float32)
+
+hidden1 = tf.nn.relu(tf.matmul(x, W1) + b1)
+hidden1_drop = tf.nn.dropout(hidden1, keep_prob)
+y = tf.nn.softmax(tf.matmul(hidden1_drop, W2) + b2)
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), 1))
+train_step = tf.train.AdagradOptimizer(0.3).minimize(cross_entropy)
+
+tf.global_variables_initializer().run()
+for i in range(3000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    train_step.run({x: batch_xs, y_: batch_ys, keep_prob: 0.75})
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
