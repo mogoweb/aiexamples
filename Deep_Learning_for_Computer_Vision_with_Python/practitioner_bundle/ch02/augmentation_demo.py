3bbd51dda8995a402b2ebb5492e046c40a59e0a4
mogoweb <mogoweb@gmail.com>
2019-01-14 22:09:04 +0800

    add sample code for data augmentation.

---
+++ Deep_Learning_for_Computer_Vision_with_Python/practitioner_bundle/ch02/augmentation_demo.py
@@ -0,0 +1,39 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import load_img
+
+import numpy as np
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to the input image")
+ap.add_argument("-o", "--output", required=True, help="path to output directory to store augmentation examples")
+ap.add_argument("-p", "--prefix", type=str, default="image", help="output filename prefix")
+args = vars(ap.parse_args())
+
+# load the input image, convert it to a NumPy array, and then
+# reshape it to have an extra dimension
+print("[INFO] loading example image ...")
+image = load_img(args["image"])
+image = img_to_array(image)
+image = np.expand_dims(image, axis=0)
+
+# construct the image generator for data augmentation then
+# initialize the total number of images generated thus far
+aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1, height_shift_range=0.1,
+                         shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode="nearest")
+total = 0
+
+# construct the actual Python generator
+print("[INFO] generating images ...")
+imageGen = aug.flow(image, batch_size=1, save_to_dir=args["output"], save_prefix=args["prefix"],
+                    save_format="jpg")
+
+# loop over examples from out image data augmentation generator
+for image in imageGen:
+  # increment out counter
+  total += 1
+
+  if total == 10:
+    break
+
