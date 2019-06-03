e28b84f7339f9ae3325cf4f83717c45dc15cc991
mogoweb <mogoweb@gmail.com>
2019-06-03 14:20:31 +0800

    add example of tfjs for WebChat mini program

---
+++ tfjs/wechat/miniprogram-test-1/tfjs/my_model.js
@@ -0,0 +1,25 @@
+import * as tf from './tf.min.js'
+
+const my_first_ai_model = epochs => {
+  // Define a model for linear regression.
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+  // Prepare the model for training: Specify the loss and the optimizer.
+  model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+
+  // Generate some synthetic data for training.
+  const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+  const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+  // Train the model using the data.
+  model.fit(xs, ys, { epochs: epochs }).then(() => {
+    // Use the model to do inference on a data point the model hasn't seen before:
+    // Open the browser devtools to see the output
+    model.predict(tf.tensor2d([5], [1, 1])).print();
+  });
+}
+
+module.exports = {
+  my_first_ai_model: my_first_ai_model
+}
